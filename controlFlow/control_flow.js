c0d9a7a18ab9045f9c8dbc1410dd40354fe35581
Sylvestre <silisipro@gmail.com>
2024-12-29 23:50:36 -0500

    tp

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -48,3 +48,44 @@ let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 console.log("Authentication Status:", authenticationStatus);
+
+function canAccessServiceDietary(role) {
+  console.log("Vérification du rôle : ", role);
+
+  switch (role.toLowerCase()) {
+    case 'employé':
+      console.log("Rôle détecté : Employé");
+      console.log("En tant qu'employé, vous avez accès aux 'Services diététiques'.");
+      break;
+
+    case 'membre inscrit':
+      console.log("Rôle détecté : Membre inscrit");
+      console.log("En tant que membre inscrit, vous avez accès aux 'Services diététiques' et à une interaction individuelle avec un diététicien.");
+      break;
+
+    case 'abonné':
+      console.log("Rôle détecté : Abonné");
+      console.log("En tant qu'abonné, vous avez un accès partiel aux 'Services diététiques'.");
+      break;
+
+    case 'non-abonné':
+      console.log("Rôle détecté : Non-abonné");
+      console.log("Vous devez d'abord vous inscrire ou vous abonner pour accéder aux 'Services diététiques'.");
+      break;
+
+    default:
+      console.log("Rôle détecté : Non reconnu");
+      console.log("Rôle non reconnu. Veuillez entrer un rôle valide.");
+  }
+
+  console.log("Fin de la vérification pour le rôle :", role);
+}
+
+console.log("Début de la vérification des rôles");
+canAccessServiceDietary("Employé");
+canAccessServiceDietary("Membre inscrit");
+canAccessServiceDietary("Abonné");
+canAccessServiceDietary("Non-abonné");
+canAccessServiceDietary("Administrateur");
+console.log("Fin de la vérification des rôles");
+
